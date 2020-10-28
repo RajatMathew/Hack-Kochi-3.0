908e60c38863c01b1df46ab64327f12dc0591011
Rajat Mathew <rajatxmathew@gmail.com>
2020-10-28 14:21:33 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var date = new Date();
 setInterval( () => {
     var date = new Date();
-    document.getElementById("timer").innerText = "The clock is ticking! It's " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " right now.";
+    var h = date.getHours();
+    if(date.getHours() >= 13){h -= 12;}
+    document.getElementById("timer").innerText = "The clock is ticking! It's " + h + ":" + date.getMinutes() + ":" + date.getSeconds() + " right now.";
 }, 1000);
