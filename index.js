ef99e15b852d6bb3e1589e51ecec6b93e1bc66ca
Rajat Mathew <rajatxmathew@gmail.com>
2020-10-28 12:15:40 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-var date = new Date();
-setInterval( () => {
-    var date = new Date();
-    document.getElementById("timer").innerText = "The clock is ticking! It's " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " right now.";
-}, 1000);
