7f06a36d962b4d684fcaec742328ebbd7848e017
Rajat Mathew <rajatxmathew@gmail.com>
2020-10-28 14:25:23 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ var date = new Date();
 setInterval( () => {
     var date = new Date();
     var h = date.getHours();
+    var s = date.getMinutes();
     if(date.getHours() >= 13){h -= 12;}
+    if(date.getMinutes() <=9){s = "0" + s.toString();}
     document.getElementById("timer").innerText = "The clock is ticking! It's " + h + ":" + date.getMinutes() + ":" + date.getSeconds() + " right now.";
 }, 1000);
