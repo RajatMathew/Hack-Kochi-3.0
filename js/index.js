2c8d1403828c42abfe9e44800d3d1731efaf3980
Rajat Mathew <rajatxmathew@gmail.com>
2020-10-29 16:13:25 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("Nov 8, 2020 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
