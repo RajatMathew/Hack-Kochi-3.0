a6425c98d1b219c64f3e69d920bc3a1c8cf70866
Rajat Mathew <rajatxmathew@gmail.com>
2020-10-30 14:19:04 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,43 @@
+var q = 0;
+var txt = 'Make. Break. Innovate';
+var speed = 100;
+document.getElementById("mbi").innerHTML = " ";
+
+
+function typeWriter() {
+
+  if (q < txt.length) {
+	  if(document.getElementById("mbi").innerHTML == " Make." || document.getElementById("mbi").innerHTML == " Make. Break.")
+{
+wait(200);
+}
+    document.getElementById("mbi").innerHTML += txt.charAt(q);
+    q++;
+    setTimeout(typeWriter, speed);
+  }
+  
+
+}
+typeWriter();
+
+
+
+
+
 var countDownDate = new Date("Nov 6, 2020 12:00:00").getTime();
 
+
+
+function wait(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+
+wait(2600);
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -29,35 +67,3 @@ var x = setInterval(function() {
     document.getElementById("timer").innerHTML = "The hunt has begun!";
   }
 }, 1000);
-
-
-
-
-var q = 0;
-var txt = 'Make. Break. Innovate';
-var speed = 100;
-document.getElementById("mbi").innerHTML = " ";
-
-function wait(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-function typeWriter() {
-
-  if (q < txt.length) {
-	  if(document.getElementById("mbi").innerHTML == " Make." || document.getElementById("mbi").innerHTML == " Make. Break.")
-{
-wait(200);
-}
-    document.getElementById("mbi").innerHTML += txt.charAt(q);
-    q++;
-    setTimeout(typeWriter, speed);
-  }
-  
-
-}
-typeWriter();
