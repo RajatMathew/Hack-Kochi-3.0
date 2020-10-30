ed212bb007797ad859da323889e305aed67624a5
Rajat Mathew <rajatxmathew@gmail.com>
2020-10-30 12:23:38 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Nov 8, 2020 10:00:00").getTime();
+var countDownDate = new Date("Nov 6, 2020 12:00:00").getTime();
 
 document.getElementById("timer").innerHTML = " ";
 // Update the count down every 1 second
