e8e92a7ac591731b2472653f8cfb453b6420ba5d
Rajat Mathew <rajatxmathew@gmail.com>
2020-11-01 22:21:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ typeWriter();
 
 
 
-var countDownDate = new Date("November 6 2020 11:59:00 GMT+0530").getTime();
+var countDownDate = new Date("November 6 2020 23:59:00 GMT+0530").getTime();
 
 
 
