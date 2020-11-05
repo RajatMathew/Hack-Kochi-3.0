c64002f6fda8790cd0fdcd031cd2023922b0e237
Rajat Mathew <rajatxmathew@gmail.com>
2020-11-06 00:03:16 +0530

    Corrected timeline, disabled registration

--- js/index.js
+++ js/index.js
@@ -65,6 +65,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "The hunt has begun!";
+    document.getElementById("timer").innerHTML = "&nbsp;";
   }
 }, 1000);
